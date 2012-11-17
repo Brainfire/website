84d33608ed7748064b6f84d8a61571d6c0b82f7a
Zakaria Zajac <zak@brainfi.re>
2012-11-17 02:28:50 -0800

    Updated to support bucket sub urls like /static/

--- brainfire/common/storage.py
+++ brainfire/common/storage.py
@@ -1,11 +1,13 @@
 from django.core.files.storage import get_storage_class
 from storages.backends.s3boto import S3BotoStorage
+from django.conf import settings
 
 class CachedS3BotoStorage(S3BotoStorage):
     """
     S3 storage backend that saves the files locally, too.
     """
     def __init__(self, *args, **kwargs):
+        kwargs['location'] = settings.STATIC_S3_PATH
         super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
         self.local_storage = get_storage_class(
             "compressor.storage.CompressorFileStorage")()
