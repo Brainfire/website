a02eb79c7e97283ec757277f5101defa56aa2469
Zakaria Zajac <zak@brainfi.re>
2012-11-17 03:12:07 -0800

    fixed google analytics variables

--- brainfire/common/context_processors.py
+++ brainfire/common/context_processors.py
@@ -1,14 +1,9 @@
 from django.conf import settings # import the settings file
 
 def common_settings(context):
-    # return the value you want as a dictionnary. you may add multiple values in there.
-    try:
-        google_analytics_id = settings.GOOGLE_ANALYTICS_ID
-    except AttributeError:
-        google_analytics_id = None
-    
     return {
-        'GOOGLE_ANALYTICS_ID': google_analytics_id,
+        'GOOGLE_ANALYTICS_ID': settings.GOOGLE_ANALYTICS_ID,
+        'GOOGLE_ANALYTICS_DOMAIN': settings.GOOGLE_ANALYTICS_DOMAIN,
         'DEBUG': settings.DEBUG,
         'SITE_TITLE': settings.SITE_TITLE,
         'SITE_TAGLINE': settings.SITE_TAGLINE,
