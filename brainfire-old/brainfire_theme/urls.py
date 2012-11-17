afc4df5058c604cd11247a9a2af969bce929d427
Zakaria Zajac <zak@brainfi.re>
2012-11-16 17:29:34 -0800

    Removed old mezzanine site folder

--- brainfire-old/brainfire_theme/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.base import TemplateView
-
-urlpatterns = patterns('',
-    url(r'^robots\.txt$', TextPlainView.as_view(template_name='robots.txt')),
-    url(r'^humans\.txt$', TextPlainView.as_view(template_name='humans.txt')),
-)
